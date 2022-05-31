26e2167132c55ad3c218d7c0e128410930b6fef2
psykovski-extended <dominik.lovetinsky99@gmail.com>
2022-05-31 20:16:50 +0200

    inital commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './SideBar.css';
+
+const SideBar = () => {
+    return (
+        <div className="sidebar">
+            SideBar
+        </div>
+    );
+};
+
+export default SideBar;
---
+++ src/components/mandelbrot/Mandelbrot.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './Mandelbrot.css';
+import Plot from 'react-plotly.js';
+import { GPU } from 'gpu.js';
+import { connect } from "react-redux";
+
+const Mandelbrot = () => {
+    return (
+        <div className="mandelbrot">
+            <Plot
+                data={[
+                {
+                    x: [1, 2, 3],
+                    y: [2, 6, 3],
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    marker: {color: 'red'},
+                },
+                ]}
+                layout={{autosize: true}}
+                useResizeHandler
+                style={{width: "100%", height: "100%"}}
+            />
+        </div>
+    );
+}
+
+const mapStateToProps = state => {
+    
+}
+
+export default connect(null)(Mandelbrot);
